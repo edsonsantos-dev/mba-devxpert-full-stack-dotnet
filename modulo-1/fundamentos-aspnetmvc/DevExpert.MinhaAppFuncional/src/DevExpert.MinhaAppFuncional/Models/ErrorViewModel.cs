738c5b8b15055ef59a9ca397073867645f94b4af
Edson Santos <dev@escode.net>
2025-03-20 06:17:48 -0300

    Add Identity schema and initial Razor layout structure
    This commit includes the creation of ASP.NET Core Identity migrations for user and role management. It also adds the initial Razor layout, partial views for login, and styling to set up a foundational structure for the application.

---
+++ modulo-1/fundamentos-aspnetmvc/DevExpert.MinhaAppFuncional/src/DevExpert.MinhaAppFuncional/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace DevExpert.MinhaAppFuncional.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
